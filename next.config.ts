533768f7f2b4dfecb16a0c6f76b92f51cb766050
meckfilipe1-beep <meckfilipe1@gmail.com>
2026-06-23 19:13:30 -0300

    Simplify next.config.ts by removing trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true
-  },
-  trailingSlash: true
+  images: { unoptimized: true }
 };
-
 module.exports = nextConfig;
