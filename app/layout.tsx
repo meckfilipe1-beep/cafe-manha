5660d8a09a3fac5093477fa5fab574f01533a081
Filipe <meckfilipe1@gmail.com>
2026-06-25 15:48:12 -0300

    Adiciona ícones e manifesto, atualiza dependências e remove arquivos antigos

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,35 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from 'next/head'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "Tapicuz - Café da Manhã",
   description: "Seu café da manhã saboroso e entregue na hora certa!",
+
+  // 🖼️ Ícone e imagem para o APK
+  icons: {
+    icon: "/imagens/icon.png",
+    apple: "/imagens/icon.png",
+  },
+
+  // 📱 Imagem de tela inicial/splash
+  openGraph: {
+    images: [{ url: "/imagens/splash.png" }],
+  },
 };
 
 export default function RootLayout({
@@ -23,15 +38,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html 
-      lang="pt-BR" 
+    <html
+      lang="pt-BR"
       translate="no"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <Head>
+  <link rel="icon" type="image/png" href="/imagens/icon.png" />
+  <link rel="apple-touch-icon" href="/imagens/icon.png" />
+</Head>
       <head>
-        {/* IMPEDIR CHROME DE PEDIR TRADUÇÃO */}
         <meta name="google" content="notranslate" />
         <meta httpEquiv="Content-Language" content="pt-BR" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,6 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tapicuz.admin',
+<<<<<<< HEAD
   appName: 'Tapicuz Admin',
   webDir: 'out',
   server: {
@@ -9,9 +10,24 @@ const config: CapacitorConfig = {
     cleartext: false,
     androidScheme: 'https'
   },
+=======
+  appName: 'Tapicuz da Sul',
+  webDir: 'out',
+
+  // Endereço correto que vai direto para a área de admin
+  server: {
+    url: 'https://tapicuz-admin-gujb.vercel.app/admin',
+    cleartext: true,
+    androidScheme: 'https',
+    allowNavigation: ['*'],
+    hostname: 'tapicuz-admin-gujb.vercel.app'
+  },
+
+
+>>>>>>> 3ef16fe (voltando aonde pegava)
   plugins: {
     LocalNotifications: {
-      smallIcon: "ic_stat_notification",
+      smallIcon: "mipmap/ic_launcher",
       iconColor: "#F97316"
     }
   }
